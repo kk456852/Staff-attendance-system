7de45e2261bff4be42d2e15e54eb1d4c8b260408
Charley Zhao <30900241+CharleyZhao123@users.noreply.github.com>
2019-05-11 13:07:22 +0800

    数据库以及基本的查询操作

---
+++ database/database.py
@@ -0,0 +1,154 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+import config
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config.from_object(config)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):  # 用户
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    position = db.Column(db.Integer, nullable=False)  # 职务 1-员工 2-主管 3-经理
+    gender = db.Column(db.Boolean)  # 性别 0-男 1-女
+    age = db.Column(db.Integer)
+    workStatus = db.Column(db.Integer)  # 工作状态 1-上班 2-正常休假 3-经理状态 4-下班 5-请假休假
+    departmentId = db.Column(db.Integer, db.ForeignKey('department.id'))  # 部门标号
+
+    def __init__(self, username, password, position, gender, age, workStatus, departmentId):
+        self.username = username
+        self.password = password
+        self.age = age
+        self.gender = gender
+        self.position = position
+
+        if(position == 2):
+            self.workStatus = 3
+            self.departmentId = 0
+        else:
+            self.workStatus = workStatus
+            self.departmentId = departmentId
+
+    # 这个方法作为对象的打印方法，类似于java类中 自己重写的 toString()方法，实现这个方法后，print(类) 将会执行这个方法进行打印
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Department(db.Model):  # 部门
+    __tablename__ = 'department'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10))
+    users = db.relationship('User', backref='department')  # ?
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return '<Department %i %r>' % (self.id, self.name)
+
+
+class WorkArrangement(db.Model):  # 工作安排
+    __tablename__ = 'arrangement'
+    arragementId = db.Column(db.Integer, primary_key=True)
+    staffId = db.Column(db.Integer, db.ForeignKey('user.id'))  # 员工标号
+    departmentId = db.Column(db.Integer, db.ForeignKey('department.id'))  # 部门标号
+    workDate = db.Column(db.Date, nullable=False)  # 工作日期
+    beginTime = db.Column(db.Time, nullable=False)  # 开始时间
+    endTime = db.Column(db.Time, nullable=False)  # 结束时间
+    assignment = db.Column(db.String(50))  # 工作安排
+
+    def __init__(self, staffId, departmentId, workDate, beginTime, endTime, assignment):
+        self.staffId = staffId
+        self.departmentId = departmentId
+        self.workDate = workDate
+        self.beginTime = beginTime
+        self.endTime = endTime
+        self.assignment = assignment
+
+    def __repr__(self):
+        return '<WorkArrangement %i %i>' % (self.staffId, self.departmentId)
+
+
+class SignSheet(db.Model):  # 签到表
+    __tablename__ = 'signsheet'
+    sheetId = db.Column(db.Integer, primary_key=True)
+    staffId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # 员工标号
+    type = db.Column(db.Integer, nullable=False)  # 类型 0-日常签到 1-临时加班
+    date = db.Column(db.Integer, nullable=False)
+    punchBeginTime = db.Column(db.Time)  # 签到上班时间
+    punchEndTime = db.Column(db.Time)  # 签到下班时间
+    isLate = db.Column(db.Boolean)  # 是否迟到
+
+    def __init__(self, staffId, type, date, punchBeginTime, punchEndTime, isLate):
+        self.sheetId = staffId
+        self.type = type
+        self.date = date
+        self.punchBeginTime = punchBeginTime
+        self.punchEndTime = punchEndTime
+        self.isLate = isLate
+
+    def __repr__(self):
+        return '<SignSheet %i %i>' % (self.sheetIdId, self.type)
+
+
+class Leave(db.Model):  # 请假
+    __tablename__ = 'leave'
+    leaveId = db.Column(db.Integer, primary_key=True)
+    staffId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # 员工标号
+    leaveReason = db.Column(db.String(50))
+    leaveDate = db.Column(db.Date, nullable=False)
+    leaveBeginTime = db.Column(db.Time, nullable=False)
+    leaveEndTime = db.Column(db.Time, nullable=False)
+    isLeavePermitted = db.Column(db.Integer)  # 0-未审核 1-通过审核 2-未通过审核
+
+    def __init__(self, staffId, leaveReason, leaveDate, leaveBeginTime, leaveEndTime, isLeavePermitted):
+        self.staffId = staffId
+        self.leaveReason = leaveReason
+        self.leaveDate = leaveDate
+        self.leaveBeginTime = leaveBeginTime
+        self.leaveEndTime = leaveEndTime
+        self.isLeavePermitted = isLeavePermitted
+
+    def __repr__(self):
+        return '<Leave %i %r>' % (self.staffId, self.leaveReason)
+
+
+class Report(db.Model):  # 销假
+    __tablename__='report'
+    reportId = db.Column(db.Integer, primary_key=True)
+    leaveId = db.Column(db.Integer, db.ForeignKey('leave.leaveId'), nullable=False)  # 对应的请假id
+    reportTime = db.Column(db.Time)
+
+    def __init__(self, leaveId, reportTime):
+        self.leaveId = leaveId
+        self.reportTime = reportTime
+
+    def __repr__(self):
+        return '<Report %i %i>' % (self.reportId, self.leaveId)
+
+
+class Overtime(db.Model):  # 加班
+    __tablename__ = 'overtime'
+    overtimeId = db.Column(db.Integer, primary_key=True)
+    overtimeThreshold = db.Column(db.Integer)  # 加班阈值 单位-分钟
+    staffId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # 员工标号
+    isOvertimePermitted = db.Column(db.Boolean)  # 是否准许加班 0-未审核 1-通过 2-不通过
+
+    def __init__(self, overtimeThreshold, staffId, isOvertimePermitted):
+        self.overtimeThreshold = overtimeThreshold
+        self.staffId = staffId
+        self.isOvertimePermitted = isOvertimePermitted
+
+    def __repr__(self):
+        return '<Overtime %i>' % self.overtimeId
+
+db.create_all()
+
---
+++ database/operation.py
@@ -0,0 +1,130 @@
+from database import *
+
+# # usertest = User(username='123', password='123', position='1', gender=1, age=1, workStatus=1, departmentId=1)
+# departmenttest = Department(1, 'test')
+#
+# db.session.add(departmenttest)
+#
+# db.session.commit()
+#
+
+
+# ---------------------------查询方法---------------------------------
+class UserInfo():
+    # 全表查询
+    def findAll(self):
+        return User.query.all()
+
+    # 根据ID主键查询
+    def getInfoById(self, id):
+        return User.query.get(id)
+
+    # 自定义查询条件
+    def getInfoByUserName(self, userName):
+        return User.query.filter_by(username=userName).all()
+
+
+class DepartmentInfo():
+    def findAll(self):
+        return Department.query.all()
+
+    def getInfoById(self, id):
+        return Department.query.get(id)
+
+    def getInfoByName(self, name):
+        return Department.query.filter_by(name=name).all()
+
+
+class WorkArrangementInfo():
+    def findAll(self):
+        return WorkArrangement.query.all()
+
+    def getInfoById(self, arragementId):
+        return WorkArrangement.query.get(arragementId)
+
+    def getInfoBystaffId(self, staffId):
+        return WorkArrangement.query.filter_by(staffId=staffId).all()
+
+    def getInfoBydepId(self, departmentId):
+        return WorkArrangement.query.filter_by(departmentId=departmentId).all()
+
+
+class SignSheetInfo():
+    def findAll(self):
+        return SignSheet.query.all()
+
+    def getInfoById(self, sheetId):
+        return SignSheet.query.get(sheetId)
+
+    def getInfoBystaffId(self, staffId):
+        return SignSheet.query.filter_by(staffId=staffId).all()
+
+    def getInfoBytype(self, type):
+        return SignSheet.query.filter_by(type=type).all()
+
+    def getInfoBydate(self, date):
+        return SignSheet.query.filter_by(date=date).all()
+
+
+class LeaveInfo():
+    def findAll(self):
+        return Leave.query.all()
+
+    def getInfoById(self, leaveId):
+        return Leave.query.get(leaveId)
+
+    def getInfoBystaffId(self, staffId):
+        return Leave.query.filter_by(staffId=staffId).all()
+
+    def getInfoByleaveDate(self, leaveDate):
+        return Leave.query.filter_by(leaveDate=leaveDate).all()
+
+    def getInfoByPermitted(self, permitted):
+        return Leave.query.filter_by(isLeavePermitted=permitted).all()
+
+
+class ReportInfo():
+    def findAll(self):
+        return Report.query.all()
+
+    def getInfoById(self, reportId):
+        return Report.query.get(reportId)
+
+    def getInfoByleaveId(self, leaveId):
+        return Report.query.filter_by(leaveId=leaveId).all()
+
+
+class Overtime():
+    def findalll(self):
+        return Overtime.query.all()
+
+    def getInfoById(self, overtimeId):
+        return Overtime.query.get(overtimeId)
+
+    def getInfoBystaffId(self, staffId):
+        return Overtime.query.filter_by(staffId=staffId).all()
+
+    def getInfoByThreshold(self, overtimeThreshold):
+        return Overtime.query.filter_by(overtimeThreshold=overtimeThreshold).all()
+
+    def getInfoBypermitted(self, permitted):
+        return Overtime.query.filter_by(isOvertimePermitted=permitted).all()
+
+
+
+# # 复杂查询以及多表关联查询DEMO展示
+# class UtilsQuery():
+#     def find(self):
+#         # 这里通过关联User和Token两张表进行关联查询
+#         return User.query.filter(User.id == Token.user_id).filter(Token.user_cname == 'XXXX').all()
+#         # return db.session.query(User).filter(User.id == Token.user_id).filter(Token.user_cname=='XXXX').all()
+#
+#     def findByPage(self, pageNum, size):
+#         # 这里展示分页查询
+#         if pageNum < 1:
+#             pageNum = 1
+#         if size < 1:
+#             size = 2
+#         start = (pageNum - 1) * size
+#         end = pageNum * size
+#         return User.query.slice(start, end).all()
