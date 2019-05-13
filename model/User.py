5d662af2aa68acdbc2b35456ffaaacebfc6c763b
JimmyMoo <mujinming666@outlook.com>
2019-05-13 16:54:07 +0800

    补充了User里；login（），增加了isManager（）。

--- model/User.py
+++ model/User.py
@@ -1,21 +1,27 @@
+from database.operation import *
+
+
 class User:
     def __init__(self, ID, password):
         self.ID = ID
         self.password = password
 
     def login(self):
-        pass
+        dPassword = UserInfo().getPasswordByUserName(self.ID)
+        if(dPassword == self.password):
+            return "success"
+        else:
+            return "fail"
 
+    def isManager(self):
+          dIdentity = UserInfo().getIdentityByUserName(self.ID)
+            # dIdentity为用户身份是否为经理，1表示经理，2表示非经理（普通员工和主管）
+             if(dIdentity == 1)：
+                return True
+            else:
+                return False
     def logout(self):
         pass
 
     def updateUser(self):
         pass
-
-
-
-
-
-
-
-
