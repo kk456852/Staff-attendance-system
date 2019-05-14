85c9c25120f72b631303674de84660d046b5286a
kk456852 <36686544+kk456852@users.noreply.github.com>
2019-05-14 15:27:00 +0800

    登陆登出
    简单实现登陆登出

--- url/auth.py
+++ url/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,request,session,json,redirect,url_for
-from model import User
+from model.User import User
 bp = Blueprint('auth',__name__,url_prefix='/auth')
 
 
@@ -7,35 +7,58 @@ bp = Blueprint('auth',__name__,url_prefix='/auth')
 def log_Index():
     return "This is auth index page!"
 
-@bp.route('/login',methods = ["POST"])
+@bp.route('/login',methods = ('GET','POST'))
 def login():
-    user = User(request.Userid,request.Password)
+    request_data = json.loads(str(request.get_data(),'utf-8'))
+    user = User(request_data['UserId'],request_data['password'])
     if user.login() is "success":
         try:
             session[request.Userid] = user.isManager
         except:
-            return {
+            response_data = {
                 'result': True,
-                'status': 201  #账户已登陆，返回到相应页面
+                'status': 201  #账户之前已登陆，返回到相应页面
             }
-        return {
-            'result' :  True,
-            'status' :  200
+            response_data = json.dumps(response_data)
+            return response_data
+        response_data = {
+            'result': True, #正常登陆
+            'status': 200
         }
+        response_data = json.dumps(response_data)
+        return response_data
     else :
-        return {
-            'result': False,
-            'status': 500    #错误代码给与区分，例如账号不存在或者密码不对
+        response_data = {
+            'result': False,# 登陆失败
+            'status': 500
         }
+        response_data = json.dumps(response_data)
+        return response_data
 
 @bp.route('/logout',methods = ('GET','POST'))
 def logout():
-
-    session.pop(request.form['user_id'], None)
-    return redirect(url_for(log_Index))
+    request_data = json.loads(str(request.get_data(), 'utf-8'))
+    try:
+        session.pop(request_data['UserId'], None)
+        response_data = {
+            'result': True,
+            'status': 200
+        }
+        #退出登陆成功
+    except:
+        response_data = {
+            'result' : False,
+            'status' : 500 #退出登陆失败
+        }
+    response_data = json.dumps(response_data)
+    return response_data
 
 
 def loged_Veri(id = None):
     #检查会话是否存在，不存在跳转到log_index界面
-    #返回身份ID  0：普通员工， 1：主管   2：经理
-    return id
+    #返回身份ID  0：普通员工， 1：主管   2：经理 3:未查询到
+    try :
+        result = session[id]
+    except:
+        result = 3
+    return result
