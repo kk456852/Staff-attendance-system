50d3b737e8e871f31a6efb1fc992a5c72e6aacf6
JimmyMoo <mujinming666@outlook.com>
2019-06-09 10:01:40 +0800

    arranges

--- app/api/arranges.py
+++ app/api/arranges.py
@@ -6,7 +6,7 @@ from .util import failed, url, success, Role
 bp = Blueprint('arranges', __name__, url_prefix='/arranges')
 
 
-@bp.route('/', methods=['GET', 'POST', 'PUT'])
+@bp.route('/director', methods=['GET', 'POST', 'PUT'])
 @url
 def arranges():
     if request.method == 'GET':#根据ID查看
@@ -19,3 +19,24 @@ def arranges():
         w.update(info)
         return success("主管更改某员工的工作安排")
 
+@bp.route('/staff', methods=['GET', 'POST', 'PUT'])
+@url
+def arrangesStaff():
+    if request.method == 'GET':#根据ID查看
+        staffID = request.args.get('staffID')
+        staffArrengement = WorkArrangement.ByID(staffID)
+        return success(staffArrengement.dict())
+
+
+@bp.route('/manager', methods=['GET', 'POST', 'PUT'])
+def arrangesManger():
+    if request.method == 'GET':#根据ID查看
+        staffID = request.args.get('staffID')
+        staffArrengement = WorkArrangement.ByID(staffID)
+        return success(staffArrengement.dict())
+
+    elif request.method == 'POST':#经理更改某员工的工作安排
+        info = request.get_json()
+        w = WorkArrangement.ByID(info['staffId'])
+        w.update(info)
+        return success("主管更改某员工的工作安排")
