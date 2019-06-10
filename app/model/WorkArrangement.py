4e1a16eb0fc9aa4bf1b940ed6c5aa64e48b2479e
JimmyMoo <mujinming666@outlook.com>
2019-06-10 10:52:29 +0800

    1

--- app/model/WorkArrangement.py
+++ app/model/WorkArrangement.py
@@ -45,4 +45,33 @@ class WorkArrangement(db.Model):  # 工作安排
     def getInfoBydepID(departmentID):
         return WorkArrangement.query.filter_by(departmentID=departmentID).all()
 
+    @staticmethod
+    def ByID(ID):
+        """根据ID构造对象
+        数据库中没有此ID时抛出异常
+
+        :returns User
+        :raise UserNotFoundException
+        """
+        w = WorkArrangement.query.get(ID)
+        return w
+
+    def update_db(self):
+        """将修改后的对象，或者新增的对象添加/修改到数据库中。
+        失败时抛出异常。
+
+        :raise InvalidRequestError
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_db(self):
+        """删除数据库中该对象对应的用户。
+        失败时抛出异常。
+
+        :raise InvalidRequestError
+        """
+        db.session.delete(self)
+        db.session.commit()
+
 
